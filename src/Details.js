f208586f2e2a043c8303a9cab0887f5ed4a52810
Rutvik Jasani <rutvik.jasani@propvivo.com>
2023-11-10 12:55:48 +0530

    my portfolio

---
+++ src/Details.js
@@ -0,0 +1,179 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/techstack/COMLogo.jpg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+import typescript from "./assets/techstack/Typescript_logo_2020.png";
+import nextJs from "./assets/techstack/nextjs.png";
+import MUI from "./assets/techstack/Mui.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Rutvik Jasani",
+  tagline: "passionate Front-End Devloper",
+  img: profile,
+  about: `I'm Frontend Web Developer,equiped with an experience of two years and a potent skill set, proficient in Next.js, React.js, JavaScript, TypeScript, Tailwind CSS, Bootstrap, Material-UI (MUI), Redux Toolkit (RTK),  HTML, and CSS.  I specialize in creating stunning and responsive web applications. With a keen eye for design and cutting-edge technology, I can transform concepts into polished, high-performance digital experiences. Let me bring your web projects to life with my expertise and flair..`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/",
+  github: "https://www.github.com/",
+  twitter: "https://twitter.com/",
+  instagram: "https://www.instagram.com/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Frontend Web Developer",
+    Company: `PropVIVO`,
+    Location: "Surat",
+    Type: "Full Time",
+    Duration: "Jul 2023 - Present",
+  },
+  {
+    Position: "Frontend Web Developer",
+    Company: `Flex Team`,
+    Location: "Surat",
+    Type: "Full Time",
+    Duration: "Aug 2022 - Jun 2023",
+  },
+  {
+    Position: "Internship",
+    Company: `Flex Team`,
+    Location: "Surat",
+    Type: "Internship",
+    Duration: "May 2022 - Jul 2022",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Master of Computer Application`",
+    Company: "Jain (Deemed-to-be University)",
+    Location: "Distant",
+    Type: "Full Time",
+    Duration: "Jan 2022 - Present",
+  },
+  {
+    Position: "Bachelor of Computer Applications",
+    Company: `C. B. PATEL COMPUTER COLLEGE`,
+    Location: "Surat",
+    Type: "Full Time",
+    Duration: "Aug 2019 - 2022",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  typescript: typescript,
+  nextJs: nextJs,
+  MUI:MUI,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Project title 1",
+    image: projectImage1,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 2",
+    image: projectImage2,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 3",
+    image: projectImage3,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 4",
+    image: projectImage4,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 5",
+    image: projectImage5,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 6",
+    image: projectImage6,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "ritikjasani119@gmail.com",
+  phone: "+91 87358 00946",
+};
